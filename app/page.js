57544bbbd81ef6aa546536ab55f4ecfaa70900f6
Sachin Agrawal <sachin@gmail.com>
2023-10-16 10:59:10 +0530

    Fix

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import {getData} from "@/pages/api/get-files";
 import Home from '@/components/Home';
 import UnderConstruction from '@/components/UnderConstruction'
-let LIVE = false
+let LIVE = process.env.LIVE === 'true'
 export default async function HomePage() {
     if (!LIVE) {
         return <UnderConstruction />
